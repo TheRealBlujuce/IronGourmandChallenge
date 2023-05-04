dd02d64ed08c2c4600299a6ef623f6b9181181d3
WillMarda <WillMarda@outlook.com>
2023-05-04 17:25:26 -0600

    Update

--- javascript/lazyload.js
+++ javascript/lazyload.js
@@ -1,10 +1,11 @@
+
 // lazy load the cards
 
 const cards = document.querySelectorAll('.card');
 
 const cardOptions = {
-  threshold: 1,
-  rootMargin: '0px 0px 175px 0px'
+  threshold: 0.5,
+  rootMargin: '0px 0px 0px 0px'
 };
 
 const cardObserver = new IntersectionObserver(entries => {
@@ -25,52 +26,52 @@ cards.forEach(card => {
   cardObserver.observe(card);
 });
 
-/// Lazy load the images
-// const images = document.querySelectorAll("[data-src]");
+// Lazy load the images
+const images = document.querySelectorAll("[data-src]");
 
-// const imgOptions = {
-//     threshold: 1,
-//     rootMargin: "0px 0px 300px 0px"
-// }
+const imgOptions = {
+    threshold: 0.5,
+    rootMargin: "0px 0px 0px 0px"
+}
 
-// function preLoadImage(img)
-// {
-//     const src = img.getAttribute("data-src");
-//     if (!src)
-//     {
-//         console.log("no source found!")
-//         return;
-//     }
+function preLoadImage(img)
+{
+    const src = img.getAttribute("data-src");
+    if (!src)
+    {
+        console.log("no source found!")
+        return;
+    }
 
-//     img.src = src;
+    img.src = src;
 
-//     // Add transition from the placeholder to the real image
-//     img.addEventListener('load', () => {
-//         console.log("Loading Image!")
-//         img.classList.add('transition', 'fade-in');
-//         img.classList.remove('opacity-0');
-//         });
+    // Add transition from the placeholder to the real image
+    img.addEventListener('load', () => {
+        console.log("Loading Image!")
+        img.classList.add('transition', 'fade-in');
+        img.classList.remove('opacity-0');
+        });
     
-//         // Remove the data-src attribute to avoid loading the image again
-//         img.removeAttribute('data-src');
-//         // Stop Observing the image once loaded.
-//         ImageObserver.unobserve(img);
-// }
+        // Remove the data-src attribute to avoid loading the image again
+        img.removeAttribute('data-src');
+        // Stop Observing the image once loaded.
+        ImageObserver.unobserve(img);
+}
 
-// const ImageObserver = new IntersectionObserver((entries, ImageObserver) => {
-//     entries.forEach(entry => {
-//         if (!entry.isIntersecting)
-//         {
-//             return;
-//         }
-//         else
-//         {
-//             preLoadImage(entry.target);
-//             ImageObserver.unobserve(entry.target);
-//         }
-//     })
-// }, imgOptions);
+const ImageObserver = new IntersectionObserver((entries, ImageObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting)
+        {
+            return;
+        }
+        else
+        {
+            preLoadImage(entry.target);
+            ImageObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
 
-// images.forEach(image =>{
-//    ImageObserver.observe(image); 
-// })
+images.forEach(image =>{
+   ImageObserver.observe(image); 
+})
